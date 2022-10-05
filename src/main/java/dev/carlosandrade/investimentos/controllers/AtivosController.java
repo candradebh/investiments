eaa3e34bad72e1601a9e6ce99eef840d4765f411
Carlos Andrade <dev_carlos@outlook.com>
2022-10-05 17:10:53 -0300

    Não permitir criar objeto repetido

--- src/main/java/dev/carlosandrade/investimentos/controllers/AtivosController.java
+++ src/main/java/dev/carlosandrade/investimentos/controllers/AtivosController.java
@@ -3,9 +3,12 @@ package dev.carlosandrade.investimentos.controllers;
 import dev.carlosandrade.investimentos.entity.Ativo;
 import dev.carlosandrade.investimentos.repository.AtivoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/ativos")
@@ -20,19 +23,31 @@ public class AtivosController {
     }
 
     @PostMapping
-    public ResponseEntity create (@RequestBody Ativo ativo) {
+    public ResponseEntity create(@Validated @RequestBody Ativo ativo) {
+
+        //Ativo ativoBuscado = repository.findByTicker(ativo.getTicker()).stream().findFirst().orElse(null);
+        Optional<Ativo> ativoBuscado = repository.findByTicker(ativo.getTicker());
+
+        if (ativoBuscado.isPresent()) {
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("O  ativo " + ativo.getTicker() + " ja esta cadastrado no sistema");
+        }
+
         return ResponseEntity.ok(repository.save(ativo));
+
+
     }
 
-    @GetMapping ("{ticker}")
-    public ResponseEntity read (@PathVariable String ticker){
-      return ResponseEntity.ok(repository.findByTicker(ticker));
+    @GetMapping("{ticker}")
+    public ResponseEntity read(@PathVariable String ticker) {
+        return ResponseEntity.ok(repository.findByTicker(ticker));
     }
 
     @PutMapping("{ticker}")
-    public ResponseEntity update (@PathVariable String ticker, @Validated @RequestBody  Ativo ativo)  {
-        return repository.findByTicker(ticker).map(existingAtivo->{
-            Ativo updatedAtivo = existingAtivo ;
+    public ResponseEntity update(@PathVariable String ticker, @Validated @RequestBody Ativo ativo) {
+        return repository.findByTicker(ticker).map(existingAtivo -> {
+            Ativo updatedAtivo = existingAtivo;
             updatedAtivo.setNome(ativo.getNome());
             updatedAtivo.setCnpj(ativo.getCnpj());
             return ResponseEntity.ok().body(repository.save(updatedAtivo));
@@ -42,7 +57,7 @@ public class AtivosController {
     @DeleteMapping("{ticker}")
     public ResponseEntity deletePostById(@PathVariable String ticker) {
 
-        repository.findByTicker(ticker).map (existingAtivo  -> {
+        repository.findByTicker(ticker).map(existingAtivo -> {
 
             repository.delete(existingAtivo);
 
