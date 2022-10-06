8b0727b4e3990cfe086294f363d0b038b150ad80
Carlos Andrade <dev_carlos@outlook.com>
2022-10-06 15:29:44 -0300

    Criando o front end

---
+++ src/frontend/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // https://cli.vuejs.org/config/#devserver-proxy
+  transpileDependencies: true,
+  devServer:{
+    port:3000,
+    proxy:{
+        '/api':{
+            target: 'http://localhost:8080',
+            ws: true,
+            changeOrigin: true
+        }
+    }
+  }
+}
